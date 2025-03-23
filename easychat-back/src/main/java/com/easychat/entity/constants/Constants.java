f0148b438cf86356a3d0ca540c6173f70e77d33f
shaoqiuyuavailable <rangsic2963277@163.com>
2025-03-23 19:20:01 +0800

    end:登陆注册2第14分钟

---
+++ easychat-back/src/main/java/com/easychat/entity/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.easychat.entity.constants;
+
+public class Constants {
+
+    public static final String REDIS_KEY_CHECK_CODE = "easychat:checkCode:";
+    public static final Integer REDIS_TIME_CHECK_1MIN = 60;
+
+
+}
