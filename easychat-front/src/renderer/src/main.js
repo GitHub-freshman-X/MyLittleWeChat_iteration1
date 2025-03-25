4e9b0c1440672ce1f24a182528c1503a1e1a0115
XuHaoran <2149403021@qq.com>
2025-03-25 20:24:57 +0800

    login-register1

--- easychat-front/src/renderer/src/main.js
+++ easychat-front/src/renderer/src/main.js
@@ -1,11 +1,30 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+
+import 'element-plus/dist/index.css'
+import '@/assets/cust-elementplus.scss'
+
+import '@/assets/icon/iconfont.css'
+import '@/assets/base.scss'
+
+import Utils from '@/utils/Utils.js'
+import Verify from '@/utils/Verify.js'
+import Request from '@/utils/Request.js'
+import Message from '@/utils/Message.js'
+import Api from '@/utils/Api.js'
+
 import router from '@/router'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
+
+app.config.globalProperties.Utils = Utils;
+app.config.globalProperties.Verify = Verify;
+app.config.globalProperties.Request = Request;
+app.config.globalProperties.Message = Message;
+app.config.globalProperties.Api = Api;
+
 app.mount('#app')
