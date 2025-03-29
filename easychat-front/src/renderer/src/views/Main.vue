bf216f50cd4e27ff1ca82196a4c0675334c1dea9
linyuanmos <2286212868@qq.com>
2025-03-29 20:48:08 +0800

    temp

---
+++ easychat-front/src/renderer/src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="body">主界面</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.body {
+  background: purple;
+  height: calc(100vh);
+}
+</style>
