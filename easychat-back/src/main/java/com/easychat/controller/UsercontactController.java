5c6e73b2aa33206a29af31d6561e47727242c629
linyuanmos <2286212868@qq.com>
2025-03-30 22:58:48 +0800

    temp

---
+++ easychat-back/src/main/java/com/easychat/controller/UsercontactController.java
@@ -0,0 +1,13 @@
+package com.easychat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/contact")
+public class UsercontactController extends ABaseController {
+
+
+}
