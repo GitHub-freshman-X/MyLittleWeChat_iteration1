f4bbefa3d19a3fcbddcba73530475577a7e17033
linyuanmos <2286212868@qq.com>
2025-04-01 23:55:00 +0800

    version2-test
    4/1

--- easychat-back/src/main/java/com/easychat/entity/po/GroupInfo.java
+++ easychat-back/src/main/java/com/easychat/entity/po/GroupInfo.java
@@ -53,6 +53,10 @@ public class GroupInfo implements Serializable {
 	 */
 	private Integer status;
 
+	/**
+	 * 成员数
+	 */
+	private Integer memberCount;
 
 	public void setGroupId(String groupId){
 		this.groupId = groupId;
@@ -113,5 +117,9 @@ public class GroupInfo implements Serializable {
 	@Override
 	public String toString (){
 		return "群ID:"+(groupId == null ? "空" : groupId)+"，群组名:"+(groupName == null ? "空" : groupName)+"，群主id:"+(groupOwnerId == null ? "空" : groupOwnerId)+"，创建时间:"+(createTime == null ? "空" : DateUtil.format(createTime, DateTimePatternEnum.YYYY_MM_DD_HH_MM_SS.getPattern()))+"，群公告:"+(groupNotice == null ? "空" : groupNotice)+"，0:直接加入 1:管理员同意后加入:"+(joinType == null ? "空" : joinType)+"，状态 1:正常 0:解散:"+(status == null ? "空" : status);
+	}
+
+	public void setMemberCount(Integer memberCount) {
+		this.memberCount = memberCount;
 	}
 }
