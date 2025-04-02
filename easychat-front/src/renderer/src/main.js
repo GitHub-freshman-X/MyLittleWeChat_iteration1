d901d74a6c258a6f15941a3b383540baa29ca096
linyuanmos <2286212868@qq.com>
2025-04-02 19:01:48 +0800

    temp

--- easychat-front/src/renderer/src/main.js
+++ easychat-front/src/renderer/src/main.js
@@ -8,6 +8,10 @@ import '@/assets/cust-elementplus.scss'
 
 import '@/assets/icon/iconfont.css'
 import '@/assets/base.scss'
+import WinOp from "@/components/WinOp.vue"
+import Layout from "@/components/Layout.vue"
+import ContentPanel from "@/components/ContentPanel.vue"
+
 
 import Utils from '@/utils/Utils.js'
 import Verify from '@/utils/Verify.js'
@@ -22,6 +26,11 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
 app.use(Pinia.createPinia());
+
+app.component('WinOp', WinOp)
+app.component('Layout', Layout)
+app.component('ContentPanel', ContentPanel)
+
 app.config.globalProperties.Utils = Utils;
 app.config.globalProperties.Verify = Verify;
 app.config.globalProperties.Request = Request;
