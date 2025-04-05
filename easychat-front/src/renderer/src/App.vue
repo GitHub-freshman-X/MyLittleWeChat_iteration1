5078b79eb8812af41abda803a9c9971394b53b11
Jenrimark <149380472+Jenrimark@users.noreply.github.com>
2025-04-05 12:41:05 +0800

    项目构建

---
+++ easychat-front/src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Versions from './components/Versions.vue'
+</script>
+
+<template>
+  <div>wd</div>
+</template>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
